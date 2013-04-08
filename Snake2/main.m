dd4f6dc9b04ab06edea91512e0c1007bece78d13
Sander Vispoel <blukkie@Sander-Vispoels-MacBook-Pro.local>
2013-04-08 16:08:51 +0200

    Initial Commit

---
+++ Snake2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snake2
+//
+//  Created by Sander Vispoel on 4/8/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
